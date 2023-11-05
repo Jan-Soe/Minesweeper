9d2504f85d429cc9f778d60fe69bcb780a26c4b7
Jan-Soe <janso@gmx.net>
2023-11-05 18:39:22 +0100

    added variables and hidden map create function

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,1 +1,21 @@
-print("Minesweeper")
+import random as r
+class Map:
+    def __init__(self,size_x,size_y,bomb_percent):
+        self.size_x=size_x
+        self.size_y=size_y
+        self.bomb_number=int(bomb_percent/100.0*size_x*size_y)
+    def create_map(self):
+        bomb_placer = self.bomb_number
+        self.true_map = {}
+        while bomb_placer > 0:
+            bomb_placer-=1
+            self.true_map["{x},{y}".format(x=r.randint(0,self.size_x),y=r.randint(0,self.size_y))]=True
+        
+        for x in range(self.size_x):
+            for y in range(self.size_y):
+                if "{x},{y}".format(x=x,y=y) not in self.true_map:
+                    self.true_map["{x},{y}".format(x=x,y=y)]= False
+        sorted(self.true_map.keys())
+        return dict(sorted(self.true_map.items()))
+map1 = Map(10,10,3)
+print(map1.create_map())
